d4fb7dd9301e344eabf08e7497a38445e019e31d
AjayPrajapat <ajayprajapat@live.com>
2024-09-03 09:11:26 +0530

    FEAT: added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.use(helmet());
+
+  const config = new DocumentBuilder()
+    .setTitle('Project Automation API')
+    .setDescription('API documentation for project automation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
